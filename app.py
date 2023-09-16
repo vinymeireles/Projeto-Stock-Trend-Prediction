422a4e6aee41d6b9aed7d4b6fe4de0a0f32a64b8
Vinicius Meireles <96025444+vinymeireles@users.noreply.github.com>
2023-09-15 21:41:09 -0300

    v5

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ model = load_model('keras_model.h5')
 
 #Testing Part
 past_100_days = data_training.tail(100)
-final_df = past_100_days.append(data_testing, ignore_index=True)
+final_df = past_100_days.pd.append(data_testing, ignore_index=True)
 input_data = scaler.fit_transform(final_df)
 
 x_test = []
